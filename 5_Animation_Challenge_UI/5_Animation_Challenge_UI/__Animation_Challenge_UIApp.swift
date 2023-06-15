a730b20971e36de4c5cdb7ff0cd3d7f788c8afcc
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-06-15 22:26:25 +0800

    swiftui
    feat:init 5

---
+++ 5_Animation_Challenge_UI/5_Animation_Challenge_UI/__Animation_Challenge_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Animation_Challenge_UIApp.swift
+//  5_Animation_Challenge_UI
+//
+//  Created by work on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct __Animation_Challenge_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
