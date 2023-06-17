a264fbe9c82c4757f7732ff91803d09bf5a81278
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-06-17 15:31:28 +0800

    feat: 06. add resource

--- A_06_Apple Maps Bottom Sheet/A_06_Apple Maps Bottom Sheet/model/Album.swift
+++ A_06_Apple Maps Bottom Sheet/A_06_Apple Maps Bottom Sheet/model/Album.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 
+struct Album: Identifiable {
+    var id = UUID().uuidString
+    var name: String
+    var image: String
+    var liked: Bool = false
+}
+
+var albums:[Album] = [
+    Album(name: "Positions", image: "Album1"),
+    Album(name: "The Best", image: "Album2",liked: true),
+    Album(name: "My Everything", image: "Album3"),
+    Album(name: "Yours Truly", image: "Album4", liked: true),
+    Album(name: "Sweetener", image: "Album5"),
+    Album(name: "Rain On Me", image: "Album6"),
+    Album(name: "Struck With U", image: "Album7", liked:true),
+    Album(name: "7 rings", image: "Album8"),
+    Album(name: "Bang Bang", image: "Album9"),
+]
+
 
 
