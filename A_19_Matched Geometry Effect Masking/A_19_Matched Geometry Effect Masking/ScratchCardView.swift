9b23bd22a0755a553d4e1fbd40b37577deab8090
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-06-25 16:12:04 +0800

    feat: 19. 基本的mask 实现

--- A_19_Matched Geometry Effect Masking/A_19_Matched Geometry Effect Masking/ScratchCardView.swift
+++ A_19_Matched Geometry Effect Masking/A_19_Matched Geometry Effect Masking/ScratchCardView.swift
@@ -24,6 +24,8 @@ struct ScratchCardView<Content: View, Overlay: View>: View {
     }
     
     
+    @State var dragPoints:[CGPoint] = []
+    
     var body: some View {
         // 逻辑很简单
         // 我们根据拖动位置一点一点 mask 内容和遮罩
@@ -32,8 +34,16 @@ struct ScratchCardView<Content: View, Overlay: View>: View {
             
             content
                 .mask {
-                    
+                    PointShape(points: dragPoints)
+                    //使用 stroke 以便它为每个点应用圆
+                        .stroke(style: StrokeStyle.init(lineWidth: pointSize, lineCap: .round, lineJoin: .round))
                 }
+                .gesture(
+                    DragGesture().onChanged({ value in
+                        // MARK: adding points
+                        dragPoints.append(value.location)
+                    })
+                )
         }
     }
 }
@@ -41,5 +51,26 @@ struct ScratchCardView<Content: View, Overlay: View>: View {
 struct ScratchCardView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+// MARK: Custom Path Shape based on drag locations
+struct PointShape: Shape {
+    var points:[CGPoint]
+    
+    // MARK: since we need animation
+    var animatableData: [CGPoint] {
+        get {points}
+        set {points = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            if let first = points.first {
+                path.move(to: first)
+                path.addLines(points)
+            }
+        }
     }
 }
