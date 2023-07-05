c9c21d26332eb0507f88418881bb2e652aee6bf4
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-07-05 14:12:27 +0800

    feat: 34. 创建响应式布局项目

---
+++ A_34_Responsive UI Design/A_34_Responsive UI Design/A_34_Responsive_UI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_34_Responsive_UI_DesignApp.swift
+//  A_34_Responsive UI Design
+//
+//  Created by Kan Tao on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct A_34_Responsive_UI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A_34_Responsive UI Design/A_34_Responsive UI Design/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  A_34_Responsive UI Design
+//
+//  Created by Kan Tao on 2023/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
