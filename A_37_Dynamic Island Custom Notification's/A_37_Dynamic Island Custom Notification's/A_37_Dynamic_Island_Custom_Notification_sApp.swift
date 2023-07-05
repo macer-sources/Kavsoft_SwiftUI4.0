36c33d83d21206dcdae1d2f4ad53837b15977bcf
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-07-05 18:12:07 +0800

    feat: 34. 初始化通知权限

---
+++ A_37_Dynamic Island Custom Notification's/A_37_Dynamic Island Custom Notification's/A_37_Dynamic_Island_Custom_Notification_sApp.swift
@@ -0,0 +1,51 @@
+//
+//  A_37_Dynamic_Island_Custom_Notification_sApp.swift
+//  A_37_Dynamic Island Custom Notification's
+//
+//  Created by Kan Tao on 2023/7/5.
+//
+
+import SwiftUI
+import UserNotifications
+
+@main
+struct A_37_Dynamic_Island_Custom_Notification_sApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// MARK: App Delegate to listen for in app notifications
+class AppDelegate: NSObject, UIApplicationDelegate,UNUserNotificationCenterDelegate  {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        if UIApplication.shared.haveDynamicIsland {
+            return [.sound]
+        }else {
+            return [.sound, .banner]
+        }
+    }
+}
+
+
+extension UIApplication {
+    var haveDynamicIsland:Bool {
+        return deviceName == "iPhone 14 Pro" || deviceName == "iPhone 14 Pro Max"
+    }
+    var deviceName: String {
+        return UIDevice.current.name
+    }
+}
+
+
+
