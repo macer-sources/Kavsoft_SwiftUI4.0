1b500e3675f7b496accfab534026623ebe51d796
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-07-06 11:27:17 +0800

    feat: 41. 通过 mask 实现渐变效果

--- A_41_Metaball Animation Canvas TimelineView/A_41_Metaball Animation Canvas TimelineView/ContentView.swift
+++ A_41_Metaball Animation Canvas TimelineView/A_41_Metaball Animation Canvas TimelineView/ContentView.swift
@@ -25,38 +25,47 @@ struct ContentView: View {
 extension ContentView {
     @ViewBuilder
     func SingleMetaBall() -> some View {
-        Canvas { context , size  in
-            // change here if you need custom color
-            // TODO: 这里才是决定最终颜色的地方，和 Ball 内部设置颜色无关
-            context.addFilter(.alphaThreshold(min: 0.5 , color: .yellow))
-            context.addFilter(.blur(radius: 30))
-            
-            context.drawLayer { ctx in
-                // MARK: Placing Symbols
-                for index in [1, 2] {
-                    if let resolvedView = context.resolveSymbol(id: index) {
-                        ctx.draw(resolvedView, at: CGPoint.init(x: size.width / 2, y: size.height / 2))
+        Rectangle()
+            .fill(.linearGradient(colors: [
+                .red,
+                .orange,
+                .yellow,
+            ], startPoint: .top, endPoint: .bottom))
+            .mask {
+                
+                Canvas { context , size  in
+                    // change here if you need custom color
+                    // TODO: 这里才是决定最终颜色的地方，和 Ball 内部设置颜色无关
+                    context.addFilter(.alphaThreshold(min: 0.5 , color: .yellow))
+                    context.addFilter(.blur(radius: 30))
+                    
+                    context.drawLayer { ctx in
+                        // MARK: Placing Symbols
+                        for index in [1, 2] {
+                            if let resolvedView = context.resolveSymbol(id: index) {
+                                ctx.draw(resolvedView, at: CGPoint.init(x: size.width / 2, y: size.height / 2))
+                            }
+                        }
                     }
+                    
+                }symbols: {
+                    Ball()
+                        .tag(1)
+                    Ball(offset: dragOffset)
+                        .tag(2)
                 }
             }
-            
-        }symbols: {
-            Ball()
-                .tag(1)
-            Ball(offset: dragOffset)
-                .tag(2)
-        }
-        .gesture(
-            DragGesture()
-                .onChanged({ value  in
-                    dragOffset = value.translation
-                })
-                .onEnded({ _ in
-                    withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
-                        dragOffset = .zero
-                    }
-                })
-        )
+            .gesture(
+                DragGesture()
+                    .onChanged({ value  in
+                        dragOffset = value.translation
+                    })
+                    .onEnded({ _ in
+                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                            dragOffset = .zero
+                        }
+                    })
+            )
     }
     
     
