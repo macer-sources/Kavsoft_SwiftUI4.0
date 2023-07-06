b9cddf0a9ded07f6643a6b5125f9854f67f8369c
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-07-06 11:47:26 +0800

    feat: 41. 基于TimelineView实现动画

--- A_41_Metaball Animation Canvas TimelineView/A_41_Metaball Animation Canvas TimelineView/ContentView.swift
+++ A_41_Metaball Animation Canvas TimelineView/A_41_Metaball Animation Canvas TimelineView/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
     // MARK: Animation properties
     @State var dragOffset: CGSize = .zero
+    @State var startAnimation: Bool = false
     
     
     var body: some View {
         VStack {
-            SingleMetaBall()
+            ClubbedView()
         }
         .preferredColorScheme(.dark)
     }
@@ -23,6 +24,57 @@ struct ContentView: View {
 
 
 extension ContentView {
+    // MARK: Clubbed one
+    // like blob background animation
+    @ViewBuilder
+    func ClubbedView() -> some View {
+        TimelineView(.animation(minimumInterval: 3.6, paused: false)) { _ in
+            Canvas { context , size  in
+                // change here if you need custom color
+                // TODO: 这里才是决定最终颜色的地方，和 Ball 内部设置颜色无关
+                context.addFilter(.alphaThreshold(min: 0.5 , color: .yellow))
+                context.addFilter(.blur(radius: 30))
+                
+                context.drawLayer { ctx in
+                    // MARK: Placing Symbols
+                    for index in 0...15 {
+                        if let resolvedView = context.resolveSymbol(id: index) {
+                            ctx.draw(resolvedView, at: CGPoint.init(x: size.width / 2, y: size.height / 2))
+                        }
+                    }
+                }
+                
+            }symbols: {
+                ForEach(0...15, id: \.self) { index in
+                    // MARK: 根据时间生成自定义offset
+                    // 将是随机的，并会相互碰撞
+                    let _offset = CGSize.init(width: .random(in: -180...180), height: .random(in: -240...240))
+                    let offset = startAnimation ? _offset : .zero
+                    ClubbedRoundedRectangle(offset: offset).tag(index)
+                }
+            }
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            startAnimation.toggle()
+        }
+    }
+    
+    @ViewBuilder
+    func ClubbedRoundedRectangle(offset: CGSize = .zero) -> some View {
+        RoundedRectangle(cornerRadius: 30)
+            .fill(.white)
+            .frame(width: 120, height: 120)
+            .offset(offset)
+        //MARK: Adding animation[less than timelineView Refresh rate]
+            .animation(.easeInOut(duration: 4), value: offset)
+    }
+    
+}
+
+
+extension ContentView {
+    // MARK: SINGLE METABALL ANIMATION
     @ViewBuilder
     func SingleMetaBall() -> some View {
         Rectangle()
