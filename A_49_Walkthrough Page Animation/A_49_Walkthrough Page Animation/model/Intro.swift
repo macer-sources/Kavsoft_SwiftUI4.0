d8e88bfcc8ff39713388de2cbf5c18298fcef52f
macer-sources <71051274+macer-sources@users.noreply.github.com>
2023-07-10 16:57:53 +0800

    feat: 49. 完善 ======= ✅

--- A_49_Walkthrough Page Animation/A_49_Walkthrough Page Animation/model/Intro.swift
+++ A_49_Walkthrough Page Animation/A_49_Walkthrough Page Animation/model/Intro.swift
@@ -15,8 +15,9 @@ struct Intro: Identifiable {
 
 
 var sample_datas:[Intro] = [
-    .init(image: "Image 1", title: "Relax"),
-    .init(image: "Image 1", title: "Care"),
-    .init(image: "Image 1", title: "Mood Dairy"),
+    .init(image: "image1", title: "Relax"),
+    .init(image: "image2", title: "Care"),
+    .init(image: "image3", title: "Mood Dairy"),
 ]
 
+let dummpText = "In this Video I'm going to teach how to create Stylish App Intro Animation's  Using SwiftUI 4.0"
--- A_49_Walkthrough Page Animation/A_49_Walkthrough Page Animation/view/IntroView.swift
+++ A_49_Walkthrough Page Animation/A_49_Walkthrough Page Animation/view/IntroView.swift
@@ -11,13 +11,15 @@ struct IntroView: View {
     
     // MARK: Animation Properties
     @State var showWalkThroughScreens: Bool = false
-    
+    @State var currentIndex: Int = 0
     var body: some View {
         ZStack {
             Color("bg")
                 .ignoresSafeArea()
             
             IntroScreen()
+            
+            WalkThroughScreens()
             
             NavBar()
         }
@@ -69,7 +71,11 @@ struct IntroView: View {
     func NavBar() -> some View {
         HStack {
             Button {
-                showWalkThroughScreens.toggle()
+                if currentIndex > 0 {
+                    currentIndex -= 1
+                }else {
+                    showWalkThroughScreens.toggle()
+                }
             } label: {
                 Image(systemName: "chevron.left")
                     .font(.title3)
@@ -91,6 +97,93 @@ struct IntroView: View {
         .offset(y: showWalkThroughScreens ? 0 : -120)
     }
 }
+
+
+// MARK: WalkThrough screen
+extension IntroView {
+    @ViewBuilder
+    func WalkThroughScreens() -> some View {
+        GeometryReader { proxy in
+            let size = proxy.size
+            
+            ZStack {
+                
+                ForEach(sample_datas.indices, id:\.self) { index in
+                    ScreenView(size: size, index: index)
+                }
+                
+                WelcomeView()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(alignment: .bottom) {
+                Image(systemName: "chevron.right")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 55, height: 55)
+                    .foregroundColor(.white)
+                    .background {
+                        RoundedRectangle(cornerRadius: 30)
+                            .fill(Color("blue"))
+                    }
+                    .onTapGesture {
+                        currentIndex += 1
+                    }
+                    .offset(y: -90)
+            }
+            .offset(y: showWalkThroughScreens ? 0 : size.height)
+        }
+    }
+}
+
+
+extension IntroView {
+    @ViewBuilder
+    func ScreenView(size: CGSize, index: Int) -> some View {
+        let intro = sample_datas[index]
+        
+        VStack(spacing: 10) {
+            Text(intro.title)
+                .font(.system(size: 28))
+            // MARK: Applying offset for each screen's
+                .offset(x: -size.width * CGFloat(currentIndex - index))
+            // MARK: Adding Animation
+            // adding delay to element's based on index
+            // my delay starts from top you can also modify code to start delay from bottom
+            // 0.2, 0.1, 0
+            // adding extra o.2 for current index
+                .animation(.interactiveSpring(response: 0.9, dampingFraction: 0.8, blendDuration: 0.5).delay(currentIndex == index ? 0.2 : 0).delay(currentIndex == index ? 0.2 : 0), value: currentIndex)
+            
+            Text(dummpText)
+                .font(.system(size: 14))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 30)
+                .offset(x: -size.width * CGFloat(currentIndex - index))
+                .animation(.interactiveSpring(response: 0.9, dampingFraction: 0.8, blendDuration: 0.5).delay(0.1).delay(currentIndex == index ? 0.2 : 0), value: currentIndex)
+            
+            Image(intro.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 250, alignment: .top)
+                .padding(.horizontal, 20)
+                .offset(x: -size.width * CGFloat(currentIndex - index))
+                .animation(.interactiveSpring(response: 0.9, dampingFraction: 0.8, blendDuration: 0.5).delay(currentIndex == index ? 0 : 0.2).delay(currentIndex == index ? 0.2 : 0), value: currentIndex)
+        }
+        
+        
+    }
+    
+    @ViewBuilder
+    func WelcomeView() -> some View {
+        VStack(spacing: 10) {
+            // 这里搞一些图片和文字
+            
+        }
+    }
+    
+    
+}
+
+
 
 struct IntroView_Previews: PreviewProvider {
     static var previews: some View {
